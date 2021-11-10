44f682245704d1a02009a1fe563941bd78cb7deb
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2021-11-10 15:07:00 -0700

    ADDS: test case for json response

--- tests/app.test.js
+++ tests/app.test.js
@@ -7,4 +7,10 @@ describe("Server is working", () => {
     const response = await request(app).get("/ping").send();
     expect(response.statusCode).toBe(200);
   });
+
+  //Making sure we are sending back json response
+  test("Pinging and expecting response to be json", async () => {
+    const response = await request(app).get("/ping").send();
+    expect(response.type).toBe("application/json");
+  });
 });
