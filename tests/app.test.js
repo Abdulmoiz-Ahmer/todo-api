4f586c2a07c2ce5e1caecf362bd3ed9e584771cf
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2021-11-10 13:37:07 -0700

    ADDS: endpoint for testing server

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,1 +1,10 @@
-const { app } = require("../src/drivers/webservers/index");
+const { app } = require("../src/drivers/webservers/app");
+const supertest = require("supertest");
+
+describe("Server is working", () => {
+  //Ping test endpoint
+  test("Pinging", async () => {
+    const response = await request(app).get("/ping").send();
+    expect(response.statusCode).toBe(200);
+  });
+});
