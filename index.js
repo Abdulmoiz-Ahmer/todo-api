7b08a2835195bfd2b759f9fecb619df40e3c9d56
Abdulmoiz <abdulmoizahmer1996@gmail.com>
2021-11-13 13:49:35 -0700

    port fetched from config

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+const config = require("./src/config/index");
 const server = require("./src/drivers/webservers/app.js");
-server.listen(4000, () => {
+server.listen(config.PORT, () => {
   console.log("Server is running on port 4000");
 });
