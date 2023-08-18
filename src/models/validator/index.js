62c384a7964fc55ed3c3aafaea0a86c47a0e5dc7
abdulmoiz-ahmer <abdulmoizahmer1996@gmail.com>
2023-08-18 12:41:47 +0500

    function refactor

--- src/models/validator/index.js
+++ src/models/validator/index.js
@@ -1,12 +1,16 @@
 let validator = (schema) => (payload) => {
   let { error } = schema.validate(payload);
   if (error) {
-    let message = error.details.map((el) => el.message).join("\n");
+    let message = error.details.map((el) => el.message).join('\n');
     return {
       error: message,
+      status: false,
     };
   }
-  return true;
+  return {
+    error: undefined,
+    status: true,
+  };
 };
 
 module.exports = validator;
